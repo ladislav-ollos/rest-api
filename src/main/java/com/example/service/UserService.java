cdceeb8d10b1e60c3375fe78e4290b49d1d91bbf
Ladislav Ollos <40268373+ladislav-ollos@users.noreply.github.com>
2021-06-06 16:54:29 +0200

    docs: added javadoc for user service

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -9,6 +9,10 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * @author Ladislav
+ *
+ */
 @Component
 public class UserService {
     private final UserRepository userRepository;
@@ -17,6 +21,12 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Get users by ID
+     * @param id - {@link User#id}
+     * 
+     * @return {@link User}
+     */
     public User getUser(Long id) {
         Optional<User> product = userRepository.findById(id);
         if (product.isPresent()) {
@@ -24,9 +34,21 @@ public class UserService {
         }
         throw new NotFoundException();
     }
+    /**
+     * List all users
+     * 
+     * @return list of {@link User}
+     */
     public List<User> getUsers() {
         return userRepository.findAll();
     }
+    
+    /**
+     * Saves the user. Generates a new {@link User#id}
+     * @param user - the {@link User} to save
+     * 
+     * @return {@link User}
+     */
     public User saveUser(User user) {
         return userRepository.save(user);
     }
