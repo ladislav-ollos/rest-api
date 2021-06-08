97fe00247627384c7c9e85167a4b11fa9f203058
Ladislav Ollos <40268373+ladislav-ollos@users.noreply.github.com>
2021-06-08 09:54:04 +0200

    docs: added javadoc for ProductService

--- src/main/java/com/example/service/ProductService.java
+++ src/main/java/com/example/service/ProductService.java
@@ -10,6 +10,10 @@ import com.example.schema.Product;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * @author Ladislav
+ *
+ */
 @Component
 public class ProductService {
     private final ProductRepository productRepository;
@@ -17,7 +21,14 @@ public class ProductService {
     public ProductService(@Autowired ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
-
+    
+    /**
+     * Get product by ID if present
+     * @param id - {@link Subscription#id}
+     * @throws {@link NotFoundException}
+     * 
+     * @return {@link Product}
+     */
     public Product getProduct(Long id) {
         Optional<Product> product = productRepository.findById(id);
         if (product.isPresent()) {
@@ -25,10 +36,21 @@ public class ProductService {
         }
         throw new NotFoundException();
     }
-
+    
+    /**
+     * Get the list of all products.
+     * 
+     * @return list of {@link Product}
+     */
     public List<Product> getProducts() {
         return productRepository.findAll();
     }
+    
+    /**
+     * Save a product.
+     * 
+     * @return {@link Product}
+     */
     public Product saveProduct(Product product) {
         return productRepository.save(product);
     }
